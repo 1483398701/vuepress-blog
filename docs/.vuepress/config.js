3cbb5c847ff58e28cc718ddf5e7e5f8b8ceffdaa
夢瓛 <1483398701@qq.com>
2021-06-22 16:04:49 +0800

    修改baseURL

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const pluginConf = require("./config/pluginConf");
 module.exports = {
   title: "夢瓛-随记",
   description: "夢瓛的博客",
-  base: process.env.NODE_ENV == 'development' ? "/" : "/blog/",
+  base: process.env.NODE_ENV == 'development' ? "/" : "/vuepress-blog/",
   head: headConf,
   plugins: pluginConf,
   markdown: {
